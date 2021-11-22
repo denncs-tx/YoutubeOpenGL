883e9f55c58f34f7adf4cb1fbca053605cddd522
denncs-tx <denncs@verizon.net>
2021-11-22 12:14:14 -0600

    Shaders section complete.

--- Main.cpp
+++ Main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <stb/stb_image.h>
 
 #include "shaderClass.h"
 #include "VAO.h"
 #include "VBO.h"
 #include "EBO.h"
+#include "Texture.h"
 
 int main()
 {
@@ -21,22 +23,20 @@ int main()
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Vertices coordinates for the triangle
+	// Vertices coordinates
 	GLfloat vertices[] = {
-	//                     COORDINATES                   /     COLORS          //
-		-0.5f,     -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f, // Lower left corner
-		0.5f,      -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f, // Lower right corner
-		0.0f,       0.5f * float(sqrt(3)) * 2 / 3, 0.0f,   1.0f, 0.6f,  0.32f, // Upper corner
-		-0.5f / 2,  0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f, 0.45f, 0.17f, // Inner left
-		0.5f / 2,   0.5f * float(sqrt(3)) / 6,     0.0f,   0.9f, 0.45f, 0.17f, // Inner right
-		0.0f,      -0.5f * float(sqrt(3)) / 3,     0.0f,   0.8f, 0.3f,  0.02f  // Inner down
+	//       COORDINATES    /     COLORS        //
+		-0.5f, -0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // Lower left corner
+		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 1.0f, // Lower right corner
+		 0.5f,  0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // Upper corner
+		 0.5f, -0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 0.0f  // Inner left
 	};
 
+	// Indices for vertices order
 	GLuint indices[] =
 	{
-		0, 3, 5,  // Lower left triangle
-		3, 2, 4,  // Lower right triangle
-		5, 4, 1   // Upper triangle
+		0, 2, 1,  // Upper triangle
+		0, 3, 2   // Lower triangle
 	};
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
@@ -72,8 +72,9 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
@@ -91,6 +92,10 @@ int main()
 	// Swap the back buffer with the front buffer
 	glfwSwapBuffers(window);
 
+	// Texture
+	Texture popCat("pop_cat.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	popCat.texUnit(shaderProgram, "tex0", 0);
+
 	// Main while loop
 	while (!glfwWindowShouldClose(window)) {
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -98,11 +103,13 @@ int main()
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
 		glUniform1f(uniID, 0.5f);
+		popCat.Bind();
+		//glBindTexture(GL_TEXTURE_2D, texture);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw the triangle using GL_TRIANGLES buffer
 		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		// Take care of all GLFW events
@@ -113,6 +120,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	popCat.Delete();
 	shaderProgram.Delete();
 
 	// Make sure that we properly destroy the window that has been created.
