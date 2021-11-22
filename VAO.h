628051fb91026f3e519e686c798ea8723ace87bc
denncs-tx <denncs@verizon.net>
2021-11-22 10:12:39 -0600

    Shaders exercise completed.

--- VAO.h
+++ VAO.h
@@ -7,12 +7,18 @@
 class VAO
 {
 public:
+	// ID reference for the Vertext Array Object
 	GLuint ID;
+	// Constructor that generates a VAO ID
 	VAO();
 
-	void LinkVBO(VBO VBO, GLuint layout);
+	// Links a VBO to the VAO using a certain layout
+	void LinkAttrib(VBO VBO, GLuint layout, GLuint numComponents, GLenum type, GLsizeiptr stride, void* offset);
+	// Binds the VAO
 	void Bind();
+	// Unbinds the VAO
 	void Unbind();
+	// Deletes the VAO
 	void Delete();
 };
 
